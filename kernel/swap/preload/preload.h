3fd15c18f98cee047ae326d8cec4542019835878
Hyeongsik Min <hyeongsik.min@samsung.com>
2015-11-24 13:52:50 +0900

    kernel: swap-modules: remove swap-modules codes
    swap-modules will be loaded as a kernel module
    
    Change-Id: I28d8d5c9fc7f2f8cb218f8b59bd322899db51232
    Signen-off-by: Hyeongsik Min <hyeongsik.min@samsung.com>

--- kernel/swap/preload/preload.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PRELOAD__
-#define __PRELOAD__
-
-#define PRELOAD_PREFIX "SWAP_PRELOAD: "
-#define PRELOAD_MAX_ATTEMPTS 10
-#define PRELOAD_DEFAULT_PERMS (S_IRUSR | S_IWUSR) /* u+rw */
-
-#endif /* __PRELOAD__ */
--- kernel/swap/uprobe/arch/x86/swap-asm/swap_sc_patch.h
+++
@@ -1,2 +0,0 @@
-void swap_depatch_syscall(void);
-void swap_patch_syscall(void);
